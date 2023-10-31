c7db8065e453a587956040d21abc093f0dac43ce
Yubin Xiao <yubin.xiao@columbia.edu>
2023-10-31 02:39:13 -0400

    Updated __init__.py with function documentation

--- src/prime_yx2764/__init__.py
+++ src/prime_yx2764/__init__.py
@@ -1,3 +1,33 @@
+"""
+The prime_yx2764 package provides utility functions to work with prime numbers.
+
+Functions:
+- is_prime: Check if a number is prime.
+
+is_prime(n):
+    """
+    Check if a number is prime.
+
+    Parameters:
+    - n (int): The number to check.
+
+    Returns:
+    - bool: True if the number is prime, False otherwise.
+
+    Example:
+    >>> is_prime(5)
+    True
+    >>> is_prime(4)
+    False
+    """
+    
+"""
+
+from .prime_yx2764 import is_prime
+
+
+
+
 from .prime_yx2764 import is_prime
 
 
